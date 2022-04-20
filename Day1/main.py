bda0a38709cc5fc0813988092ede5c35489ef700
Edson Ballestas <eballestas94@gmail.com>
2022-04-20 03:55:40 +0000

    Adding Exercise day 1

---
+++ Day1/main.py
@@ -0,0 +1,4 @@
+print("Welcome to BandGen!")
+place = input("Where are you from?... (please dont be from malambo) ")
+pet = input("whats the name of your pet?... ")
+print(f"Your Band name is... los {pet} de {place}")
