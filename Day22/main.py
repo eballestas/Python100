13579da90b94b5728c6fbe7ef24093f77c1e430e
Edson Ballestas@asus-ubuntu <eballestas94@gmail.com>
2022-09-13 00:17:08 -0400

    adding day 22 code

--- Day22/main.py
+++ Day22/main.py
@@ -1,5 +1,7 @@
+import time
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
 
 screen = Screen()
 screen.setup(width=800, height=600)
@@ -9,7 +11,7 @@ screen.tracer(0)
 
 l_paddle = Paddle((350, 0))
 r_paddle = Paddle((-350, 0))
-
+ball = Ball()
 
 screen.listen()
 screen.onkey(l_paddle.go_up, "Up")
@@ -20,6 +22,18 @@ screen.onkey(r_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
+
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce()
+
+    print(f"distance {ball.distance(r_paddle)}")
+    print(f"xcor{ball.xcor()}")
+
+    #if ball.distance(r_paddle) < 50 and ball.xcor() > 340:
+    #    print("contact")
+
 
 screen.exitonclick()
